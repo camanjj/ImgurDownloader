cea740254166ffc89bca0894bdf27195e00c1c2a
Cameron Jackson <jackson.cameron82@gmail.com>
2018-02-02 18:37:07 -0500

    Initial Commit

---
+++ ImgFinder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ImgFinder
+//
+//  Created by Cameron Jackson on 2/2/18.
+//  Copyright © 2018 Cameron Jackson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
